de975fba05dedb9ef9d41039315e307d2f5aff40
Roger Guimaraes <logroger@gmail.com>
2020-12-23 12:23:45 -0300

    New Directions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
 eDirection dir;
 
 void Setup()
@@ -20,7 +28,7 @@ void Setup()
 void Draw()
 {
     system("clear"); //for Windows system("cls");
-    for (int i = 0; i < width+2; i++)
+    for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
 
@@ -28,27 +36,69 @@ void Draw()
     {
         for (int j = 0; j < width; j++)
         {
-            if (j==0)
+            if (j == 0)
                 cout << "#";
-                         
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
                 cout << " ";
-           
-            if (j == width - 1)       
+
+            if (j == width - 1)
                 cout << "#";
         }
         cout << endl;
     }
-    
-    for (int i = 0; i < width+2; i++)
+
+    for (int i = 0; i < width + 2; i++)
         cout << "#";
-    cout << endl;    
+    cout << endl;
 }
 void Input()
 {
+    if (_kbhit())
+    {
+        sitch(_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic()
 {
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
 }
 
 int main()

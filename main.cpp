58cbf0a08477ab1924d78060d1aa31a2667fe711
Roger Guimaraes <logroger@gmail.com>
2020-12-23 14:10:28 -0300

    Tabuleiro infinito

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+//#include "curses.h" 
 #include <conio.h>
+//#include "unistd.h"
+//#include <ncurses.h>
 using namespace std;
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
 enum eDirection
 {
     STOP = 0,
@@ -43,7 +49,25 @@ void Draw()
             else if (i == fruitY && j == fruitX)
                 cout << "F";
             else
-                cout << " ";
+            {
+                bool print = false;
+                for(int k = 0; k<nTail; k++)
+                {
+                    
+                    if(tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                   
+                }
+                if(!print)
+                    cout << " ";
+                
+                
+
+            }
+                
 
             if (j == width - 1)
                 cout << "#";
@@ -54,12 +78,13 @@ void Draw()
     for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
+    cout << "SCore:" << score << endl;
 }
 void Input()
 {
     if (_kbhit())
     {
-        sitch(_getch())
+        switch(getch())
         {
         case 'a':
             dir = LEFT;
@@ -82,6 +107,23 @@ void Input()
 
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+
     switch (dir)
     {
     case LEFT:
@@ -99,11 +141,30 @@ void Logic()
     default:
         break;
     }
+    if (x > width || x < 0 || y > height || y < 0)
+        gameOver = true;
+
+    if( x >= width) x = 0; else if (x<0) x = width -1;
+    if( y >= width) y = 0; else if (y<0) y = height -1;
+
+    for(int i = 0; i <nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+
+
+    if ( x == fruitX && y == fruitY)
+    {        
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
 }
 
 int main()
 {
-    printf("Let's start a new simple Snake Game");
+    printf("Let's start a new simple Snake Game\n");
 
     Setup();
     while (!gameOver)
@@ -111,7 +172,7 @@ int main()
         Draw();
         Input();
         Logic();
-        //Sleep(10);
+        sleep(10);
     }
 
     return 0;
